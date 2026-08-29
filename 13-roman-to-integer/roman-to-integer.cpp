d2959cf9fa6ac83ffc6dfa8c1ecffaeef2bde7f4
aarohipandey <aarohipnd20@gmail.com>
2026-08-29 22:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (77.77%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int>roman={
-            {'I',1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000}       
-            };
-            int total=0;
-            int n=s.length();
-            for(int i=0;i<n;i++)
-            {
-                if(i<n-1&&roman[s[i]]<roman[s[i+1]])
-                total-=roman[s[i]];
-                else{
-                    total+=roman[s[i]];
-                }
-            }
-        return total;
+    int total = 0;
+    int n = s.length();
+    
+    // Inline values helper for fast lookup without map overhead
+    auto getValue = [](char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    };
+
+    for (int i = 0; i < n; i++) {
+        int current = getValue(s[i]);
+        
+        // If next character is larger, subtract current value instead of adding
+        if (i < n - 1 && current < getValue(s[i + 1])) {
+            total -= current;
+        } else {
+            total += current;
+        }
     }
+    
+    return total;
+}
+    
 };
